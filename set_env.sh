37cd859c9dc8eee0cfc0fab4cf35dbe77b4d878d
ngolongquan <60766273+ngolongquan@users.noreply.github.com>
2022-05-17 07:55:06 +0700

    Add files via upload

---
+++ set_env.sh
@@ -0,0 +1,13 @@
+# This file is used for convenience of local development.
+# DO NOT STORE YOUR CREDENTIALS INTO GIT
+export POSTGRES_USERNAME=postgres
+export POSTGRES_PASSWORD=postgres
+export POSTGRES_HOST=postgres.cspc8lzitqqq.ap-southeast-1.rds.amazonaws.com
+export POSTGRES_DB=postgres
+export AWS_BUCKET=quannl-udagram
+export AWS_REGION=us-east-1
+export AWS_PROFILE=default
+export JWT_SECRET=helloworld
+export URL=http://localhost:8100
+export DOCKER_USERNAME=ngolongquan
+export DOCKER_PASSWORD=abcde12345-
